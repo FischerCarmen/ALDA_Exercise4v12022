50e38c813f2746df763a60fddd8a6679e250a9e1
FischerCarmen <91220946+FischerCarmen@users.noreply.github.com>
2022-06-12 22:53:22 +0200

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -5,59 +5,81 @@
 #include <ctype.h>
 
 list *l_init() {
-    return NULL;
+    list* newList = (list*) malloc(sizeof(list));
+    newList->count = 0;
+    newList->head = nullptr;
+    return newList;
 }
 
-void l_insert(list * lst, char * word)
-{
+void l_insert(list * lst, char * word){
+    element* newHead = (element*) malloc(sizeof(element));
+    lst->count+=1;
+    strcpy(newHead->word, word);
+    newHead->next = lst->head;
+    lst->head = newHead;
     
 }
 
-void l_delete(list * lst, char * word)
-{
+void l_delete(list * lst, char * word){
+    element* target = lst->head;
+    element* before = nullptr;
+    for(; strcmp(target->word,word) != 0; target = target->next){
+        before = target;
+    }
+    if(before == nullptr){
+        lst->head=lst->head->next;
+    }else{
+        before->next=target->next;
+    }
+    lst->count-=1;
+    free(target);
     
 }
 
 void l_print(list * lst) {
+    for(element* current = lst->head; current->next != nullptr; current = current->next){
+        for(size_t size = current->count, curr = 0; curr > size; curr++){
+            printf("%c", current->word[curr]);
+        }
+         printf("\n");
+    }
 
 }
 
 element *l_find(list * lst, char * word)
 {
-    return NULL;
+    element* target = lst->head;
+    for(; strcmp(target->word,word) != 0; target = target->next){;}
+    return target;
 }
 
 int readFile(list* list, char* filename){
     // Parser von und mit Kommentaren von Herbert Paulis:
     FILE * file;
     char line[300];        // sicher lang genug fuer eine Zeile
-    
-    file = fopen(filename, "r");
-    
+    file = fopen("alice.txt", "r");
     if (!file) // bei Fehler wird NULL zurueckgegeben
     {
         printf("Fehler beim Oeffnen\n");
         return 1;
     }
-    
     while (!feof(file))    // Solange nicht Dateiende erreicht wurde
     {
         fgets(line, 300, file);
         if (isspace(line[0]))  // Eliminieren von Leerzeilen
             continue;
-        line[strcspn(line, "\r\n")] = 0; // ersetzt beliebeige Kombination von CR&LF durch 0
-        //printf("%s\n", line);  // Testausgabe
-        
+        line[strcspn(line, "\r\n")] = 0;// ersetzt beliebeige Kombination von CR&LF durch 0
+   //     printf("%s\n", line);  // Testausgabe
+  //      printf("1");
         // Alternative fuer die folgende while-schleife: C-Funktion strtok()
         // https://en.cppreference.com/w/c/string/byte/strtok
         
-        int zeilen_index = 0;   // Positionszaehler in der Zeile
+        int zeilen_index = 0; // Positionszaehler in der Zeile
         while (zeilen_index < strlen(line))
         {
             char wort[100];
             int wort_index = 0; // Positionszaehler im Wort
             int skip_extra = 0; // uebersprungene Zeichen
-            
             for (int i = zeilen_index; i < strlen(line); i++)
             {
                 if (line[i] == ' ')    // Leerzeichen
@@ -86,9 +108,12 @@ int readFile(list* list, char* filename){
             /*******************************************************************************/
             // Hier ist das jeweilige Wort in Variable wort enthalten zur spaeteren Weiterverarbeitung in der Liste
             // Achtung: wort kann Leerstring "" enthalten (z.B. wenn Zahl im Text war)!
-            //printf("%s:", wort);    // Testausgabe
+            printf("%s:", wort);    // Testausgabe
            
             element * tmp;
+            if(!l_find(list, tmp->word)){
+                l_insert(list, tmp->word);
+            }
             
             // TODO
             // Hier müssen die Elemente noch in die Liste hinzugefügt werden.
@@ -96,26 +121,70 @@ int readFile(list* list, char* filename){
             // list wird als Parameter der Funktion readFile übergeben.
             
             /*******************************************************************************/
+            if (tmp != nullptr){
+                tmp->count++;
+                continue;
+            }
+            l_insert(list, wort);
         }
-        //printf("\n");   // Nur fuer Testausgabe
+      //  printf("\n");   // Nur fuer Testausgabe
     }
     return 0;
 }
 
 void frequencyCount(list *list, int *frequencies){
-    
+    element* elem = list->head;
+    for(;elem!=nullptr;){
+        for (int i = 0; elem->word[i] != '\0'; i++) {
+            if (isalpha(elem->word[i])) {
+                frequencies[(tolower(elem->word[i])-'a')]+=elem->count;
+            }
+        }
+        elem = elem->next;
+    }
 }
 
 int letterCount(list *list){
-    return 0;
+    int frequencies[26] = { 0 };
+    frequencyCount(list, frequencies);
+    int counter = 0;
+
+
+    for (int i = 0; i < 26; i++) {
+        counter += frequencies[i];
+    }
+
+    return counter;
+    
 }
 
 int wordCount(list *list){
-    return 0;
+    element* elem = list->head;
+    int counter = 0;
+    for(;elem!=nullptr;){
+        elem = elem->next;
+        counter += elem->count;
+    }
+    return counter;
 }
 
 void deleteWords(list *list, int frequency){
     // Hinweis:
     // In unserem Fall wird für frequency z.B. 100 übergeben. Alle
     // Wörter mit einer geringeren Frequenz/Häufigkeit werden gelöscht.
+    
+    element* elem = list->head;
+    element* tmp = nullptr;
+
+    for(;elem!=nullptr;){
+        if (elem->count < frequency) {
+            tmp = elem->next;
+            l_delete(list, elem->word);
+            elem = tmp;
+        }
+        else {
+            elem = elem->next;
+        }
+        
+    }
 }
